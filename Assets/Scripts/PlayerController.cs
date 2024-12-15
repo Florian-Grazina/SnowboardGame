cf6a117dfd50e5c7a8c67d5b3cb2312e766cd9e7
Florian Grazina <Floriangrazina@gmail.com>
2024-12-15 23:29:11 +0100

    Find of type

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -1,18 +1,40 @@
+using System;
 using UnityEngine;
 
 public class PlayerController : MonoBehaviour
 {
-    [SerializeField]
-    protected float torqueAmount = 1f;
+    [SerializeField] protected float torqueAmount = 1f;
+    [SerializeField] protected float boostSpeed = 35f;
+    [SerializeField] protected float baseSpeed = 20f;
 
     protected new Rigidbody2D rigidbody2D;
+    SurfaceEffector2D surfaceEffector2D;
 
     protected void Start()
     {
-        rigidbody2D = GetComponent<Rigidbody2D>();  
+        rigidbody2D = GetComponent<Rigidbody2D>();
+        surfaceEffector2D = FindFirstObjectByType<SurfaceEffector2D>();
     }
 
     protected void Update()
+    {
+        RotatePlayer();
+        RespondToBoost();
+    }
+
+    private void RespondToBoost()
+    {
+        if(Input.GetKey(KeyCode.Space))
+        {
+            surfaceEffector2D.speed = boostSpeed;
+        }
+        else if(Input.GetKeyUp(KeyCode.Space))
+        {
+            surfaceEffector2D.speed = baseSpeed;
+        }
+    }
+
+    private void RotatePlayer()
     {
         if (Input.GetKey(KeyCode.A))
         {
