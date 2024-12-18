7da362e59fbe8b97eef863e1a724d923654f01fb
Florian Grazina <Floriangrazina@gmail.com>
2024-12-19 00:04:50 +0100

    snowboard particle

--- Assets/Scripts/DustTrail.cs
+++ Assets/Scripts/DustTrail.cs
@@ -1,16 +1,17 @@
+using System;
 using UnityEngine;
 
 public class DustTrail : MonoBehaviour
 {
     [SerializeField] protected ParticleSystem trailDustEffect;
 
-    protected void OnCollisionEnter(Collision collision)
+    protected void OnCollisionEnter2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
             trailDustEffect.Play();
     }
 
-    protected void OnCollisionExit(Collision collision)
+    protected void OnCollisionExit2D(Collision2D collision)
     {
         if (collision.gameObject.CompareTag("Ground"))
             trailDustEffect.Stop();
