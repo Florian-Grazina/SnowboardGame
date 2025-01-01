94f298792ff2f40c9c8df40a2ce078740e0936ae
Florian Grazina <Floriangrazina@gmail.com>
2025-01-01 21:57:08 +0100

    SFX

--- Assets/Scripts/FinishLine.cs
+++ Assets/Scripts/FinishLine.cs
@@ -1,18 +1,21 @@
+using System;
 using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class FinishLine : MonoBehaviour
 {
-    [SerializeField]
-    protected float reloadSceneDelay = 1;
-    [SerializeField]
-    protected ParticleSystem finishEffect;
+    [SerializeField] protected float reloadSceneDelay = 2;
+    [SerializeField] protected ParticleSystem finishEffect;
+
+    private bool isFinished;
 
     protected void OnTriggerEnter2D(Collider2D other)
     {
-        if (other.gameObject.CompareTag("Player"))
+        if (other.gameObject.CompareTag("Player") && !isFinished)
         {
+            isFinished = true;
             finishEffect.Play();
+            GetComponent<AudioSource>().Play();
             Invoke(nameof(ReloadScene), reloadSceneDelay);
         }
     }
